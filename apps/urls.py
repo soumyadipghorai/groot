7bed2cbae328a1f0b3370a8ae081d735e101310e
Soumyadip Ghorai <ghorai.soumyadip33@gmail.com>
2022-10-07 07:41:32 +0530

    home and login page added {#4]
    home, login page HTML & CSS added. other files like gitignore and requirements added

--- apps/urls.py
+++ apps/urls.py
@@ -4,7 +4,8 @@ from apps import views
 
 urlpatterns = [
     path('', views.index, name = "home"),
-    # path('about', views.about, name = "about"),
+    path('login', views.loginPage, name = "login"),
+    path('about', views.about, name = "about"),
     # path('service', views.service, name = "service"),
     # path('contact', views.contact, name = "contact")
 ]
--- apps/views.py
+++ apps/views.py
@@ -6,3 +6,9 @@ from django.contrib import messages
 # Create your views here.
 def index(request) : 
     return render(request, 'index.html')
+
+def loginPage(request) : 
+    return render(request, 'login.html')
+
+def about(request) : 
+    return render(request, 'about.html')
