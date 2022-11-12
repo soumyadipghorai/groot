b91d72fe0b5277ba1bebc1f84ae687cc1fa2b185
Soumyadip Ghorai <ghorai.soumyadip33@gmail.com>
2022-11-12 09:46:02 +0530

    root updated [#8.1]

--- app.py
+++ app.py
@@ -1,86 +1,61 @@
-from flask import Flask, flash, request, redirect, url_for, render_template
-import urllib.request
+from flask import Flask, render_template, request, session
 import os
 from werkzeug.utils import secure_filename
 import cv2
 import numpy as np
-# from rembg import remove  
-# import PIL.Image
-# if not hasattr(PIL.Image, 'Resampling'):  # Pillow<9.0
-#     PIL.Image.Resampling = PIL.Image
 
-app = Flask(__name__)
- 
 UPLOAD_FOLDER = 'static/uploads/'
-UPLOAD_FOLDER_PREPROCESSED = 'static/preprocessed/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
  
-app.secret_key = "secret key"
+app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['UPLOAD_FOLDER_PREPROCESSED'] = UPLOAD_FOLDER_PREPROCESSED
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
  
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.secret_key = 'This is your secret key to utilize session in Flask'
+
+ 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def abput():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('loginNew.html')
+
+@app.route('/Imageupload')
+def uploadPage() : 
+    return render_template('index_upload_and_display_image.html')
+ 
+@app.route('/Imageupload',  methods=("POST", "GET"))
+def uploadFile():
+    if request.method == 'POST':
+        uploaded_img = request.files['uploaded-file']
+        img_filename = secure_filename(uploaded_img.filename)
+        uploaded_img.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
+        session['uploaded_img_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+ 
+        return render_template('index_upload_and_display_image_page2.html')
+ 
 
 def color_quantization(img, k):
-    # Transform the image
     data = np.float32(img).reshape((-1, 3))
 
-    # Determine criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
 
-    # Implementing K-Means
     ret, label, center = cv2.kmeans(data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
     center = np.uint8(center)
     result = center[label.flatten()]
     result = result.reshape(img.shape)
     return result
- 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-     
- 
-# @app.route('/')
-# def home():
-#     return render_template('indexCopy.html')
 
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-@app.route('/')
-def homeindex():
-    return render_template('indexCopy.html')
+@app.route('/show_image')
+def displayImage():
+    img_file_path = session.get('uploaded_img_file_path', None)
+    return render_template('show_image.html', user_image = img_file_path)
  
-@app.route('/', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # print(file)
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-        input_image = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], file))
-        output = color_quantization(input_image, 14)
-        cv2.imshow(input_image)
-        # file.save(os.path.join(app.config['UPLOAD_FOLDER_PREPROCESSED'], output))
-
-        #print('upload_image filename: ' + filename)
-        flash('Image successfully uploaded and displayed below')
-        return render_template('indexCopy.html', filename=filename)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
- 
-@app.route('/display/<filename>')
-def display_image(filename):
-    return render_template('result.html')
- 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__=='__main__':
+    app.run(debug = True)
